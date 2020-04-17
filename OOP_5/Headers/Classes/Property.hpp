ab89547c6d7bb33e18e93f7d2ac5e7d05dea2a4d
Кирилл Курко <kirillkurko2609@gmail.com>
2020-04-17 13:53:07 +0300

    Added Property class

--- OOP_5/Headers/Classes/Property.hpp
+++ OOP_5/Headers/Classes/Property.hpp
@@ -2,7 +2,44 @@
 #define Property_hpp
 
 #include <iostream>
+#include <string>
+#include "../Utilities/Area.hpp"
 
-
+class Property {
+protected:
+    double cost;
+    std::string type;
+    std::string location;
+    Area area;
+    std::string additionalInformation;
+    std::string notes;
+public:
+    Property(double cost = 0.0,
+             std::string type = std::string(),
+             std::string location = std::string(),
+             Area area = Area(),
+             std::string additionalInformation = std::string(),
+             std::string notes = std::string());
+    Property(const Property& other);
+    virtual ~Property() = default;
+    
+    double getCost() const;
+    std::string getType() const;
+    std::string getLocation() const;
+    Area getArea() const;
+    std::string getAdditionalInformation() const;
+    std::string getNotes() const;
+    
+    void setCost(double cost);
+    void setType(std::string type);
+    void setLocation(std::string location);
+    void setArea(Area area);
+    void setAdditionalInformation(std::string additionalInformation);
+    void setNotes(std::string notes);
+    
+    virtual void printInformation() const;
+    
+    Property& operator= (const Property& other);
+};
 
 #endif
--- OOP_5/SourceCode/Classes/Property.cpp
+++ OOP_5/SourceCode/Classes/Property.cpp
@@ -1,9 +1,70 @@
-//
-//  Property.cpp
-//  OOP_5
-//
-//  Created by Кирилл Курко on 4/17/20.
-//  Copyright © 2020 Николай Курко. All rights reserved.
-//
-
 #include "Property.hpp"
+
+Property::Property(double cost,
+                   std::string type,
+                   std::string location,
+                   Area area,
+                   std::string additionalInformation,
+                   std::string notes) {
+    
+}
+
+Property::Property(const Property& other) {
+    
+}
+
+double Property::getCost() const {
+    
+}
+
+std::string Property::getType() const {
+    
+}
+
+std::string Property::getLocation() const {
+    
+}
+
+Area Property::getArea() const {
+    
+}
+
+std::string Property::getAdditionalInformation() const {
+    
+}
+
+std::string Property::getNotes() const {
+    
+}
+
+void Property::setCost(double cost) {
+    
+}
+
+void Property::setType(std::string type) {
+    
+}
+
+void Property::setLocation(std::string location) {
+    
+}
+
+void Property::setArea(Area area) {
+    
+}
+
+void Property::setAdditionalInformation(std::string additionalInformation) {
+    
+}
+
+void Property::setNotes(std::string notes) {
+    
+}
+
+void Property::printInformation() const {
+    
+}
+
+Property& Property::operator= (const Property& other) {
+    
+}
