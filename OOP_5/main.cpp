d25b6522cc4be7cb91d4a83735870614df9d7122
Кирилл Курко <kirillkurko2609@gmail.com>
2020-04-17 13:05:54 +0300

    Initial Commit

---
+++ OOP_5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP_5
+//
+//  Created by Кирилл Курко on 4/17/20.
+//  Copyright © 2020 Николай Курко. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
